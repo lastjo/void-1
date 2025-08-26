05af8acdcd81fc4e6581172ea24e084a711cb72a
GregHib <greghib@users.noreply.github.com>
2025-08-26 17:47:15 +0100

    Convert all content

---
+++ game/src/main/kotlin/content/entity/npc/combat/magic/ChaosDruid.kt
@@ -0,0 +1,16 @@
+package content.entity.npc.combat.magic
+
+import content.entity.combat.npcCombatPrepare
+import content.skill.magic.spell.Spell
+import content.skill.magic.spell.spell
+import world.gregs.voidps.engine.event.Script
+import world.gregs.voidps.type.random
+@Script
+class ChaosDruid {
+
+    init {
+        npcCombatPrepare("chaos_druid*") { npc ->
+            npc.spell = if (random.nextBoolean() && Spell.canDrain(target, "confuse")) "confuse" else ""
+        }
+    }
+}
