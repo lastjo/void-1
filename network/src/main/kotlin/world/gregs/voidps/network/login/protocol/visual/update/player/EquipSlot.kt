583d0bd18528c12eb8ff689b976c5369eb5d3e3b
GregHib <greghib@users.noreply.github.com>
2025-08-27 23:15:20 +0100

    Add more subscriber types

--- network/src/main/kotlin/world/gregs/voidps/network/login/protocol/visual/update/player/EquipSlot.kt
+++ network/src/main/kotlin/world/gregs/voidps/network/login/protocol/visual/update/player/EquipSlot.kt
@@ -18,6 +18,18 @@ enum class EquipSlot(val index: Int) {
     ;
 
     companion object {
+        const val HAT = 0
+        const val CAPE = 1
+        const val AMULET = 2
+        const val WEAPON = 3
+        const val CHEST = 4
+        const val SHIELD = 5
+        const val LEGS = 7
+        const val HANDS = 9
+        const val FEET = 10
+        const val RING = 12
+        const val AMMO = 13
+
         private val map = mapOf(
             "None" to None,
             "Hat" to Hat,
